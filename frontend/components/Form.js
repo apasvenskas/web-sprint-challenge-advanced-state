0880e88c8ee58bd7f5d27b2e750e1366a5943362
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-08-08 20:43:14 -0700

    working on preserving the state

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { inputChange, resetForm, setQuiz, postQuiz} from '../state/action-creators';
 //import * as actionCreators from '../state/action-creators'
 
 export function Form(props) {
   console.log('Form state', props.form)
+
+  // const useLocalStorage = (key, initialValue) => {
+  //   const [storedValue, setStoredValue] = React.useState(() => {
+  //     const item = localStorage.getItem(key);
+  //     return item ? JSON.parse(item) : initialValue;
+  //   });
+
+  //   useEffect(() => {
+  //     localStorage.setItem(key, JSON.stringify(storedValue));
+  //   }, [key, storedValue]);
+
+  //   return [storedValue, setStoredValue];
+  // }
+
+  // useEffect(() => {
+  //   const handleLocationChange = (location) => {
+  //     if(location.pathname === '/form') {
+  //       const storedForm = localStorage.getItem('form');
+  //       if(storedForm){
+  //         const parsedForm = JSON.parse(storedForm);
+  //         props.inputChange('question_text', parsedForm.question_text);
+  //         props.inputChange('true_answer_text', parsedForm.true_answer_text);
+  //         props.inputChange('false_answer_text_text', parsedForm.false_answer_text_text);
+  //       }
+  //     }
+  //   }
+  //   const unlisten = history.listen(handleLocationChange);
+  //   return() => unlisten(); 
+  // }, [history, props]);
+
+  
+
   const onChange = evt => { 
     let payloadChange;
     switch (evt.target.id){
@@ -33,6 +65,15 @@ export function Form(props) {
     console.log('onSubmit', 'Form submitted')
   }
   console.log('form', props.form);
+
+  const inputField = () => {
+    return (
+      props.form.question_text.trim().length === 0 ||
+      props.form.true_answer_text.trim().length === 0 || 
+      props.form.false_answer_text.trim().length === 0
+    )
+  }
+
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
@@ -40,7 +81,7 @@ export function Form(props) {
       <input maxLength={50} onChange={onChange} id="new_True_Answer" placeholder="Enter true answer" />
       <input maxLength={50} onChange={onChange} id="new_False_Answer" placeholder="Enter false answer" />
 
-      <button type='submit' id="submitNewQuizBtn">Submit new quiz</button>
+      <button type='submit' id="submitNewQuizBtn" disabled={inputField()}>Submit new quiz</button>
       {props.error && <p className="error">{props.error}</p>}
       {props.success && <p className='success'>{props.success}</p>}
     </form>
---
+++ frontend/state/historyStore.js
@@ -0,0 +1,7 @@
+
+
+
+
+// create a history store here using useLocation & useHistory!!!
+
+// Okay this is getting to complicated. Need to add one simple property to the project. the state has to remain the same while navigating through the app/webpage. That is the only thing I need to do. I cannot Change the App.js file with routes, I canot make new reducers or action-creators, and I cannot install new dependencies.
